70d16d460429070c8df9fd6acf21e13a3500fb62
zhouzehao <zhouzehao@byteluck.com>
2023-06-08 11:16:04 +0800

    TEST

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,5 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("afsfs1e1fe");
-        System.out.println("test3");
     }
 }
